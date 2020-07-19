d6655a5f629e088e2659ead9b3a0295b3af11442
Koty <Koty1209@qq.com>
2020-07-19 21:16:44 +0800

    两个线程打印日期

--- src/concurrencytools/threadlocal/ThreadLocalNormalUsage00.java
+++ src/concurrencytools/threadlocal/ThreadLocalNormalUsage00.java
@@ -1,4 +1,28 @@
 package concurrencytools.threadlocal;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 两个线程打印日期
+ */
 public class ThreadLocalNormalUsage00 {
+
+    public String date(int seconds) {
+        // 该构造方法参数单位为毫秒，从1970-01-01 00:00:00 GMT+8 起计时
+        Date date = new Date(1000 * seconds);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        return format.format(date);
+    }
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            String date = new ThreadLocalNormalUsage00().date(10);
+            System.out.println(date);
+        }).start();
+        new Thread(() -> {
+            String date = new ThreadLocalNormalUsage00().date(104707);
+            System.out.println(date);
+        }).start();
+    }
 }
